d290fbeeff5c635e128f0856f48bdb46355087b4
wangrongyi <WANG06.23@iChat.sp.edu.sg>
2024-06-30 23:19:06 +0800

    updated collection.py with new function to combine lists

--- src/collection.py
+++ src/collection.py
@@ -18,19 +18,35 @@ def collectBook(person, location, bookList):
 
             if len(borrowList[info]) < 10:
                 borrowList[info].append(tempBookList[i])
-        
-    if len(borrowList[info]) == 0: 
-        if location == 1:
-            output = "Go to Location 2"
-        elif location == 2:
-            output = "Go to Location 1"
-        lcd.lcd_display_string("Wrong Location", 1)
-        lcd.lcd_display_string(output, 2)
+    
+    if len(tempBookList) != 0:
+        if len(borrowList[info]) == 0: 
+            if location == 1:
+                output = "Go to Location 2"
+            elif location == 2:
+                output = "Go to Location 1"
+            lcd.lcd_display_string("Wrong Location", 1)
+            lcd.lcd_display_string(output, 2)
+            time.sleep(0.5)
+
+        else:
+            for i in range(len(borrowList[info])):
+                dispense.dispenseBook()
+    
+    else:
+        lcd.lcd_display_string("No reservations", 1)
         time.sleep(0.5)
 
-    else:
-        for i in range(len(borrowList[info])):
-            dispense.dispenseBook()
+    return borrowList
+
+def combineList(borrowList, tempList):
+    for info in tempList:
+        if info in borrowList:
+            for book in tempList[info]:
+                borrowList[info].append(book)
+            
+        else:
+            borrowList[info] = tempList[info]
 
     return borrowList
 
@@ -40,10 +56,15 @@ def main():
                   'Test2&7654321': [['Book 1', 'Location 1', '2024-06-09 15:34:32']]}
     person = ['Test1', '1234567']
     location = 2
+    
+    egBorrowlist = {'Test1&1234567': [['Book 3', '2024-06-15 21:30:29']],
+                    'Test2&7654321': [['Book 5', '2024-06-15 21:28:26']]}
 
     dc_motor.init()
+    tempList = collectBook(person, location, egBooklist)
+    print(tempList)
 
-    print(collectBook(person, location, egBooklist))
+    print(combineList(tempList, egBorrowlist))
 
 if __name__ == "__main__":
     main()
