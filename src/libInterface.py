06f258d24c06073a6533ce0416925a46fc251502
dionchoy <dionchoy.23@ichat.sp.edu.sg>
2024-07-09 21:17:02 +0800

    updated libInterface.py to verify userIC

--- src/libInterface.py
+++ src/libInterface.py
@@ -4,6 +4,7 @@ from threading import Thread
 import lib_loc as library
 import time
 import getBooklist
+import parseBooklist
 
 lcd = LCD.lcd()
 lcd.lcd_clear()
@@ -20,12 +21,45 @@ def setup(location):
     lcd.lcd_display_string("Press '*'", 2)
 
 def auth():
-    verified = 0
+    global bookList
+    global password
+
+    verified = False
+    restart = True
+    
     lcd.lcd_clear()
     lcd.lcd_display_string("Please scan your", 1)
     lcd.lcd_display_string("admin card      ", 2)
-    while(verified == 0):
-        adminNo = 1234567 #replace with camera
+    nameList = parseBooklist.getNameList(bookList)
+    attmps = 1
+
+    while(restart == True and attmps < 10):
+        password = 0
+        adminNo = '1234567' #replace with camera
+        response = parseBooklist.findPerson(nameList, adminNo)
+        verified = response[0]
+
+        if verified == True:
+            person = response[1]
+            lcd.lcd_clear()
+            lcd.lcd_display_string(person[0], 1)
+            lcd.lcd_display_string(person[1], 2)
+            restart = False
+            
+            return True, person
+
+        elif verified == False:
+            lcd.lcd_clear()
+            output = 'to try again ('+ str(attmps) + ')'
+            lcd.lcd_display_string("Please press '#'", 1)
+            lcd.lcd_display_string(output, 2)
+            attmps += 1
+            while(password != '#'):  
+                restart = True
+   
+            if attmps == 10:
+                lcd.lcd_clear()
+                return [False]
 
 def loc_loop():
     global password
@@ -38,7 +72,12 @@ def loc_loop():
         if password == '*':
             session = 1
             print(userLoc)
-            auth()
+            authenticate = auth()
+            if authenticate[0] == False:
+                session = 0
+
+            elif authenticate[0] == True:
+                print(authenticate[1])
 
 def getList():
     global bookList
@@ -55,6 +94,7 @@ def getList():
         if borrowList != checkChangeBorrow:
             print('borrow: ',borrowList)
             checkChangeBorrow = borrowList
+
 def main():
     global userLoc
     global password
