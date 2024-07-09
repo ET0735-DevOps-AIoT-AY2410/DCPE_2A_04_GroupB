ebdb4b6ae48c4b07cb2131ef9081eedc9b9d8a41
dionchoy <dionchoy.23@ichat.sp.edu.sg>
2024-07-09 21:13:07 +0800

    updated libInterface.py to request bookList

--- src/libInterface.py
+++ src/libInterface.py
@@ -3,6 +3,7 @@ from hal import hal_keypad as keypad
 from threading import Thread
 import lib_loc as library
 import time
+import getBooklist
 
 lcd = LCD.lcd()
 lcd.lcd_clear()
@@ -18,11 +19,42 @@ def setup(location):
     lcd.lcd_display_string(output, 1)
     lcd.lcd_display_string("Press '*'", 2)
 
+def auth():
+    verified = 0
+    lcd.lcd_clear()
+    lcd.lcd_display_string("Please scan your", 1)
+    lcd.lcd_display_string("admin card      ", 2)
+    while(verified == 0):
+        adminNo = 1234567 #replace with camera
+
 def loc_loop():
-    while(True):
+    global password
+    session = 0
+    
+    while(session == 0):
         userLoc = library.get_loc()
         setup(userLoc)
 
+        if password == '*':
+            session = 1
+            print(userLoc)
+            auth()
+
+def getList():
+    global bookList
+    global borrowList
+    checkChangeReserve = {}
+    checkChangeBorrow = {}
+    while(True):
+        data = getBooklist.getReserve()
+        bookList = data[0]
+        borrowList = data[1]
+        if bookList != checkChangeReserve:
+            print('reserve: ', bookList)
+            checkChangeReserve = bookList
+        if borrowList != checkChangeBorrow:
+            print('borrow: ',borrowList)
+            checkChangeBorrow = borrowList
 def main():
     global userLoc
     global password
@@ -35,5 +67,8 @@ def main():
     loc_thread = Thread(target=loc_loop)
     loc_thread.start()
 
+    book_thread = Thread(target=getList)
+    book_thread.start()
+    
 if __name__ == '__main__':
     main()
